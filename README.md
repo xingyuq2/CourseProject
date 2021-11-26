a8ba522d02ff47b4cdbe9bf68268b308c58911cc
Xingyu Qiu <xingyuq2@illinois.edu>
2021-11-25 20:01:10 -0600

    improve the recommender and edit the README

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # CourseProject
 
-Please fork this repository and paste the github link of your fork on Microsoft CMT. Detailed instructions are on Coursera under Week 1: Course Project Overview/Week 9 Activities.
+## Install python package
+pip install pandas\
+pip install sklearn\
+pip install nltk\
+python -m nltk.downloader stopwords\
+pip install collections\
+pip install flask\
+pip install flask-cors\
+pip install csv\
+pip install bs4\
+pip install selenium\
+pip install time
+
+## Install Node.js and npm
+Download Node.js by this link: https://nodejs.org/en/download/\
+If node and npm is not detected after downloading, try to restart your computer.
+
+
+## Usage
+1.If you want to create a new data csv file by using scraper, then follow this instruction. Note that this will rewrite videos.csv file and scraping also takes a long time (approximately 20 minutes).
+
+- Run netflix scraper in the root directory by command:\
+python -m scraper.netflix_scraper
+
+2.If you want to open the video recommender web application, then follow these instructions.
+
+- Run flask app in the root directory by command:\
+python -m flask_app
+
+- Run react app in the "video-recommender-app" directory by command:\
+cd video-recommender-app\
+npm install\
+npm start
--- recommender.py
+++ recommender.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+from collections import Counter
 
 VIDEO_CSV_FILE = 'videos.csv'
 NUMBER_VIDEO_RECOMMEND = 10
@@ -10,9 +12,20 @@ def combine_features(row):
     """
     Combine features that are used to compare silimarities to form a single string
     """
-    res = row['name'] + ' ' + row['mood_tag'] + ' ' + row['genres'] + ' ' + row['casts'] + ' ' + row['creators']
+    res = row['name'] + ' ' + row['synopsis'] + ' ' + row['mood_tag'] + ' ' + row['genres'] + ' ' + row['casts'] + ' ' + row['creators']
     res = res.replace(',', ' ')
+    res = remove_stop_words(res)
     return res
+
+
+def remove_stop_words(text):
+    """
+    Remove stop words in text and return a new string
+    """
+    stop_words = stopwords.words('english')
+    stopwords_dict = Counter(stop_words)
+    text = ' '.join([word for word in text.split() if word not in stopwords_dict])
+    return text
 
 
 def get_name_from_index(df, index):
@@ -28,7 +41,7 @@ def recommend(mov_idx):
     """
     df = pd.read_csv(VIDEO_CSV_FILE)
     # features that is used to recommend videos
-    features = ['name', 'mood_tag', 'genres', 'casts', 'creators']
+    features = ['name', 'synopsis', 'mood_tag', 'genres', 'casts', 'creators']
     # fill NA with empty string
     for feature in features:
         df[feature] = df[feature].fillna('')
